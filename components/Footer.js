0c730dad7574abadfb951a228e19378525df4d83
Jose Jovian <josejovian55@gmail.com>
2022-03-28 23:14:29 +0700

    feat: footer

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<div className="flex align-center justify-center mt-16 text-center text-gray-500 z-20">
+			&copy; 2022 Jose Jovian • Created with NextJS, Tailwind, and Framer-motion.
+		</div>
+	);
+};
+
+export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Navigation />
-			<div className="body pageify w-full pt-16 h-full">
+			<div className="body pageify w-full py-16">
 				<Component {...pageProps} />
+				<Footer />
 			</div>
 		</>
 	);
