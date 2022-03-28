b983b81b2f76f2f56fffbe0de27085dad75182b5
Jose Jovian <josejovian55@gmail.com>
2022-03-28 22:51:28 +0700

    style: small padding tweak

--- components/index/Landing.js
+++ components/index/Landing.js
@@ -5,7 +5,7 @@ const Landing = () => {
 		<div className="flex flex-col justify-center items-center text-center">
 			<h1>Hey! <b>Jose</b> here.</h1>
 			<Links />
-			<p className="mt-4 px-8 text-2xl font-light">
+			<p className="mt-4 text-2xl font-light">
 				I&nbsp;am an undergraduate computer science student, and an aspiring front-end developer.
 				I&nbsp;am particularly interested in web development, especially React.
 				I&nbsp;create personal projects to test out the things I have learnt.
