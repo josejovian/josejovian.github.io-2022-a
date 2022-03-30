bf6909b1190b46a839a250f5ae0b38b1c17de51b
Jose Jovian <josejovian55@gmail.com>
2022-03-30 18:24:55 +0700

    feat: block component

---
+++ components/Block.js
@@ -0,0 +1,9 @@
+const Block = ({ children }) => {
+	return (
+		<div className="flex flex-col justify-center items-center text-center mt-16">
+			{ children }
+		</div>
+	);
+};
+
+export default Block;
