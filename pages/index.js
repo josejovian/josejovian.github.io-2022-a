4b0f2d8db8d7befa9e9b42855b42c7a28148d67f
Jose Jovian <josejovian55@gmail.com>
2022-03-30 18:36:40 +0700

    feat: added SEO to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
 import Head from "next/head";
 import Image from "next/image";
 import Featured from "../components/index/Featured";
-import Landing from "../components/index/Landing";
+import Links from "../components/index/Links";
+import PageHead from "../components/PageHead";
+import SEO from "../components/SEO";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
 	return (
 		<>
-			<Landing />
+			<SEO
+				templateTitle="home"
+				description="An undergraduate computer science student, and an aspiring front end developer. Simply a website to showcase the projects I've built to test the knowledge I obtain."
+			/>
+			<PageHead
+				title={
+					<>
+						Hey! <b>Jose</b> here.
+					</>
+				}
+				addon={<Links />}
+				description={
+					<>
+						I&nbsp;am an undergraduate computer science student, and
+						an aspiring front-end developer. I&nbsp;am particularly
+						interested in web development, especially React.
+						I&nbsp;create personal projects to test out the things I
+						have learnt.
+					</>
+				}
+			/>
 			<Featured />
 		</>
 	);
