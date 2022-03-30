62d48cb1810cea825fa4b28541a446b8a040e0be
Jose Jovian <josejovian55@gmail.com>
2022-03-30 18:26:40 +0700

    feat: featured projects with static data

--- components/index/Featured.js
+++ components/index/Featured.js
@@ -1,44 +1,43 @@
 import FeaturedCard from "../FeaturedCard";
+import Block from "../Block";
 
-const projects = [{
-	id: "bncc-x-tiket-movies",
-	name: "BNCC x Tiket Movies",
-	techs: ["nextjs", "redux"],
-	overview: `
-	A simple project I contributed to in Frontend Development Bootcamp at BNCC Academy 2021/2022.
+const projects = [
+	{
+		id: "bncc-x-tiket-movies",
+		name: "BNCC x Tiket Movies",
+		techs: ["nextjs", "redux"],
+		overview: `
+	A project I contributed to in Frontend Development Bootcamp at BNCC Academy 2021/2022.
 	`,
-}, {
-	id: "disclone",
-	name: "Disclone",
-	techs: ["react", "redux", "firebase"],
-	overview: `
-	A simple project I made to practice state management and firebase.
+	},
+	{
+		id: "disclone",
+		name: "Disclone",
+		techs: ["react", "redux", "firebase"],
+		overview: `
+	A project I made to practice state management and firebase.
 	`,
-}, {
-	id: "quizify",
-	name: "Quizify",
-	techs: ["react", "firebase"],
-	overview: `
+	},
+	{
+		id: "quizify",
+		name: "Quizify",
+		techs: ["react", "firebase"],
+		overview: `
 	A project that I worked with my classmates for our Agile Software Development class.
 	`,
-}];
+	},
+];
 
 const Featured = () => {
-
 	const featuredElement = projects.map((feat) => {
-		return (
-			<FeaturedCard key={feat.name} {...feat} />
-		);
+		return <FeaturedCard key={feat.name} {...feat} />;
 	});
 
 	return (
-		<div className="flex flex-col justify-center items-center text-center mt-16">
+		<Block>
 			<h2>Featured Projects</h2>
-			<p className="mt-4 px-8 text-xl font-light"></p>
-			<div>
-				{ featuredElement }
-			</div>
-		</div>
+			<div>{featuredElement}</div>
+		</Block>
 	);
 };
 
