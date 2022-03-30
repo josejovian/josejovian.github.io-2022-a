0f5f51b231a10b400852a26d76c290bf16601bca
Jose Jovian <josejovian55@gmail.com>
2022-03-30 18:25:16 +0700

    feat: education and experience section

---
+++ components/about/Education.js
@@ -0,0 +1,32 @@
+import Block from "../Block";
+import ParagraphList from "../ParagraphList";
+
+const list = [
+	{
+		name: "BINUS University",
+		addon: '2019 - 2013',
+		description: `
+			I am majoring in Computer Science (Global Class).
+			I joined a student organization named Bina Nusantara
+			Computer Club (BNCC) Alam Sutera in October 2019.
+			I also participated in Indonesia National Contest 2020
+			in a team "Polyoctahedron", and placed 37th.
+		`,
+	},
+	{
+		name: "SMA Citra Berkat",
+		addon: '2016 - 2019',
+		description: "I majored in Natural Sciences.",
+	},
+];
+
+const Education = () => {
+	return (
+		<Block>
+			<h2>Education</h2>
+			<ParagraphList list={list} />
+		</Block>
+	);
+};
+
+export default Education;
---
+++ components/about/Experience.js
@@ -0,0 +1,49 @@
+import Block from "../Block";
+import ParagraphList from "../ParagraphList";
+
+const list = [
+	{
+		name: "Learning and Teaching (LnT) Division Manager in Bina Nusantara Computer Club (BNCC) Alam Sutera",
+		addon: 'October 2020 - October 2021',
+		description: (
+			<>
+				I had been managing and organizing the members' classes.
+				 I also had been the head committee of the
+				workshop <b>"Let's Learn C Programming" BNCC &times; Course-Net</b>.
+			</>
+		),
+	},
+	{
+		name: "Instructor of the C Programming Class in Bina Nusantara Computer Club (BNCC) Alam Sutera",
+		addon: 'September 2020 - January 2021',
+		description:
+			`I had been one of the two instructors that regularly
+			taught the C programming language (including basic algorithm)
+			for BNCC members, which were first semester students, for
+			one whole semester.`,
+	},
+	{
+		name: "Learning and Teaching (LnT) Division Activist in Bina Nusantara Computer Club (BNCC) Alam Sutera",
+		addon: 'October 2019 - October 2020',
+		description: (
+			<>
+				I had been a part of the committee of few events, 
+				such as <b>Branding Yourself with Social Media (Seminar)</b>&nbsp;
+				and <b>"Android for Future" Android Programming Workshop</b>.
+				I also had assisted the LnT manager at the time to do their duties
+				as a LnT manager.
+			</>
+		),
+	},
+];
+
+const Experience = () => {
+	return (
+		<Block>
+			<h2>Organizational/Volunteering Experience</h2>
+			<ParagraphList list={list} />
+		</Block>
+	);
+};
+
+export default Experience;
