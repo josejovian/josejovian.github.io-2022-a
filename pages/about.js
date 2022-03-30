2e98165a7ed3f5d59e691cdb09280e1f89c934dc
Jose Jovian <josejovian55@gmail.com>
2022-03-30 18:37:06 +0700

    feat: added SEO, education and experience to about

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,32 @@
 import Head from "next/head";
 import Image from "next/image";
+import Education from "../components/about/Education";
+import Experience from "../components/about/Experience";
+import PageHead from "../components/PageHead";
 import styles from "../styles/Home.module.css";
 
 export default function About() {
 	return (
-		<div>
-
-		</div>
+		<>
+			<SEO
+				templateTitle="about"
+				description="An undergraduate computer science student, and an aspiring front end developer. Simply a website to showcase the projects I've built to test the knowledge I obtain."
+			/>
+			<PageHead
+				title={
+					<>
+						About Me
+					</>
+				}
+				description={
+					<>
+						An undergraduate computer science student, and an
+						aspiring front-end developer.
+					</>
+				}
+			/>
+			<Education />
+			<Experience />
+		</>
 	);
 }
