1a0bfa3a0c86b434a5c772c1fe70fcfdd1e94f31
Jose Jovian <josejovian55@gmail.com>
2022-04-23 22:01:21 +0900

    style: project cards are now vertical and animated

--- pages/projects.js
+++ pages/projects.js
@@ -1,40 +1,40 @@
 import Head from "next/head";
 import Image from "next/image";
+import AnimatedDiv from "../components/generic/AnimatedDiv";
 import Card from "../components/generic/Card";
 import PageHead from "../components/page/PageHead";
 import SEO from "../components/SEO";
 import styles from "../styles/Home.module.css";
 
 const Projects = ({ projects }) => {
-
-	const projectElements = projects.map((project) =>
-		<Card key={project.title} {...project} />
-	);
+	const projectElements = projects.map((project) => (
+		<Card key={project.title} type="vertical" {...project} />
+	));
 
 	return (
 		<>
-			<SEO
-				templateTitle="projects"
-			/>
+			<SEO templateTitle="projects" />
 			<PageHead
 				title="Projects"
 				description="The projects I've contributed to."
 			/>
-			{ projectElements }
+			<AnimatedDiv className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-16">
+				{projectElements}
+			</AnimatedDiv>
 		</>
 	);
-}
+};
 
 export const getServerSideProps = async (req) => {
 	const { getProjects, extractDataFromProjects } = require("../lib/mdx");
 
 	const projects = await getProjects();
 	const data = await extractDataFromProjects(projects);
-	
+
 	return {
 		props: {
-			projects: data
-		}
+			projects: data,
+		},
 	};
 };
 
