64d77af7f079fd6e928402c2db18d8c31c0e116f
Jose Jovian <josejovian55@gmail.com>
2022-05-13 23:07:45 +0900

    style: put card styling in component from globals

--- components/generic/Card.js
+++ components/generic/Card.js
@@ -6,6 +6,10 @@ const Card = ({ children, className, ...rest }) => {
 			"card",
 			"flex justify-start items-start",
 			"text-left border rounded-md overflow-hidden",
+			"text-black transition-all font-normal",
+			"hover:bg-teal-50 hover:shadow-md",
+			"active:bg-teal-100",
+			"focus:bg-teal-100",
 			className,
 		)} {...rest}>
 			{ children }
